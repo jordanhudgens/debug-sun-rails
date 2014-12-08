fdc59163632fead07cef6d76b157592dd8dcdbed
Sterling Sun <Sterling.Sun@OnLive.com>
2014-12-08 20:23:25 +0000

    trying to fix heroku push

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Enterpriseape::Application.routes.draw do
+
+  
+  resource :items
+  
+  resource :emloyees
+  
+  resource :companies do
+    collection { post :import }
+  end
+    
   get "welcome/index"
   get "welcome/about"
   get "welcome/contact"
@@ -6,8 +16,10 @@ Enterpriseape::Application.routes.draw do
   get "welcome/pricing"
   get "welcome/features"
   
-  resources :invoices
-
+  resources :invoices do
+    resources :purchases, except: [:index], controllers: 'invoice/purchases'
+  end
+  
   root to: 'welcome#index' 
   
 end
